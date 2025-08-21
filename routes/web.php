ffd7c6b9f12f2fe6856847d9a9170cef312b6878
Chinthana144 <chinthana144@gmail.com>
2025-08-22 00:09:21 +0400

    add nav bar

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+});
+
+Route::get('/template', function () {
+    return view('layouts.template');
 });
 
 require __DIR__.'/auth.php';
